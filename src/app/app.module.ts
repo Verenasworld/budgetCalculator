a71900ec227a7c4762db0bad14c20f8e054e50ca
Verenasworld <verena.felizidas@gmail.com>
2021-04-13 16:10:04 +0200

    add budget list component and first position style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { AdditemformComponent } from './additemform/additemform.component';
+import { BudgetItemListComponent } from './budget-item-list/budget-item-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
-    AdditemformComponent
+    AdditemformComponent,
+    BudgetItemListComponent
   ],
   imports: [
     BrowserModule,
