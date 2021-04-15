2359d382766be6a1c44e95cb01ca6b6deee6676f
Verenasworld <verena.felizidas@gmail.com>
2021-04-15 16:00:44 +0200

    create an interface vor updating budget value

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BudgetItem } from 'src/shared/models/budget-item.model';
+import { UpdateEvent } from '../budget-item-list/budget-item-list.component';
 
 @Component({
   selector: 'app-main-page',
@@ -24,5 +25,11 @@ export class MainPageComponent implements OnInit {
     let index = this.budgetItems.indexOf(item);
     this.budgetItems.splice(index, 1);
     this.totalBudget -= item.amount;
+  }
+
+  updateItem(updateEvent: UpdateEvent){
+     // replace the item with the updated/ submitted item from the form 
+        //replace the old with the new updatet item
+          this.budgetItems[this.budgetItems.indexOf(updateEvent.old)] = updateEvent.new; 
   }
 }
